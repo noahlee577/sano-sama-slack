f41d885779b094b10554d519b82796977bfce653
Myung Chang Lee (Noah Lee) <20549315+noahlee577@users.noreply.github.com>
2022-05-29 04:26:31 -0700

    Update app.js
    no more self-responses, should log better the user name

--- app.js
+++ app.js
@@ -38,127 +38,129 @@ app.message('are you alive Steve', async ({ message, say }) => {
 */
 
 app.message(/(sano|steve).*/, async ({ message, context, say }) => {
-  // RegExp matches are inside of context.matches
-  // const greeting = context.matches[0];
+  if(message.user != "USLACKBOT"){
+    const choices = [
+                   'Hosano in excelsis',
+                   '*messa di voce\'ing intensifies*',
+                   ':sassy-sano:',
+                   ':master-sano:',
+                   //cool(),
+                   //'Meet me at MemSquirt',
+                   /*
+                   '@Jin-Hee ',
+                   '@Lily ',
+                   '@Seyi ',
+                   '@Lauren ',
+                   '@Becca ',
+                   '@Maya ',
+                   '@Julia ',
+                   '@Millie ',
+                   '@Toby ',
+                   '@Maïgane ',
+                   '@Esther ',
+                   '@Sarah ',
+                   '@Diego ',
+                   '@Trip ',
+                   '@Oscar ',
+                   '@Fletcher ',
+                   '@Bai-han ',
+                   */
+                   //'@Minseung can you please share your workout routine. Like dang #zaddy',
+                   //'@Ben ',
+                   //'@KeeSeok ',
+                   //'@Kyle ',
+                   //'@Ben ',
+                   //'@Kaile ',
+                   //'@Akshar ',
+                   //'@Zach ',
+                   //'@Jeremy',
+                   '',
+                   'Another reminder to send in anonymous/non-anonymous messages of Chorale shout-outs to yourmanito92@gmail.com! 😊 You can use temporary email address (like 1hr email) for anonymity!',
+                   '*plays a sick slack key vamp*',
+                   //'*cringing in Hawaiian*',
+                   //'*judging in ʻŌlelo Hawaiʻi*',
+                   //'Mmmmm that post was so ʻono! nom nom nom',
+                   'catfacts_trigger', // Will pick random cat fact
+                   'catfacts_trigger', // Mohr kat faxs
+                   '',  // empty text will indicate the need for meme-ing ;)
+                   '',  // Add a few for increased meme chances
+                   '',
+                   ''
+                  ];
+    picked_response = choices[Math.floor(Math.random() * choices.length)];
 
-  const choices = [
-                 'Hosano in excelsis',
-                 '*messa di voce\'ing intensifies*',
-                 ':sassy-sano:',
-                 ':master-sano:',
-                 //cool(),
-                 //'Meet me at MemSquirt',
-                 /*
-                 '@Jin-Hee ',
-                 '@Lily ',
-                 '@Seyi ',
-                 '@Lauren ',
-                 '@Becca ',
-                 '@Maya ',
-                 '@Julia ',
-                 '@Millie ',
-                 '@Toby ',
-                 '@Maïgane ',
-                 '@Esther ',
-                 '@Sarah ',
-                 '@Diego ',
-                 '@Trip ',
-                 '@Oscar ',
-                 '@Fletcher ',
-                 '@Bai-han ',
-                 */
-                 //'@Minseung can you please share your workout routine. Like dang #zaddy',
-                 //'@Ben ',
-                 //'@KeeSeok ',
-                 //'@Kyle ',
-                 //'@Ben ',
-                 //'@Kaile ',
-                 //'@Akshar ',
-                 //'@Zach ',
-                 //'@Jeremy',
-                 '',
-                 'Another reminder to send in anonymous/non-anonymous messages of Chorale shout-outs to yourmanito92@gmail.com! 😊 You can use temporary email address (like 1hr email) for anonymity!',
-                 '*plays a sick slack key vamp*',
-                 //'*cringing in Hawaiian*',
-                 //'*judging in ʻŌlelo Hawaiʻi*',
-                 //'Mmmmm that post was so ʻono! nom nom nom',
-                 'catfacts_trigger', // Will pick random cat fact
-                 'catfacts_trigger', // Mohr kat faxs
-                 '',  // empty text will indicate the need for meme-ing ;)
-                 '',  // Add a few for increased meme chances
-                 '',
-                 ''
-                ];
-  picked_response = choices[Math.floor(Math.random() * choices.length)];
+    if (picked_response.match('catfacts_trigger')){
+      const cat_intro = [
+                       "Everyone needs a cat fact now and again 😜 ",
+                       //"You might say, \'wow, Steve is actually hard to meme about\', and you'd be right! So here's a cat fact instead: ",
+                       "Did you know... ",
+                       "Why make Steve memes when you can read cat facts? ",
+                       "And it's time for... random cat facts! ",
+                       //"Cat Facts! 😜 ",
+                       'Oh my, would you look at the time?! It\'s time for.... a cat fact!! 😜'
+                     ];
 
-  if (picked_response.match('catfacts_trigger')){
-    const cat_intro = [
-                     "Everyone needs a cat fact now and again 😜 ",
-                     //"You might say, \'wow, Steve is actually hard to meme about\', and you'd be right! So here's a cat fact instead: ",
-                     "Did you know... ",
-                     "Why make Steve memes when you can read cat facts? ",
-                     "And it's time for... random cat facts! ",
-                     //"Cat Facts! 😜 ",
-                     'Oh my, would you look at the time?! It\'s time for.... a cat fact!! 😜'
-                   ];
+      const cat_flair = [
+                       "Meow! 😺",
+                       "Me-woah! 😸",
+                       "Me-awww! 😻",
+                       "Le Meow 😺",
+                       "yeet.",
+                       "Stev-eow!",
+                       "Aww! 🥺"
+                      ];
 
-    const cat_flair = [
-                     "Meow! 😺",
-                     "Me-woah! 😸",
-                     "Me-awww! 😻",
-                     "Le Meow 😺",
-                     "yeet.",
-                     "Stev-eow!",
-                     "Aww! 🥺"
-                    ];
-
-    await say(cat_intro[Math.floor(Math.random() * cat_intro.length)] + catfacts[Math.floor(Math.random() * catfacts.length)] + ' ' + cat_flair[Math.floor(Math.random() * cat_flair.length)]);
+      await say(cat_intro[Math.floor(Math.random() * cat_intro.length)] + catfacts[Math.floor(Math.random() * catfacts.length)] + ' ' + cat_flair[Math.floor(Math.random() * cat_flair.length)]);
+    }
+    //if an empty string response is chosen--fill with meme attachment
+    else if (!picked_response){
+      await say({
+      blocks: [
+          {
+            "type": "image",
+            "title": {
+              "type": "plain_text",
+              "text": "yaet"
+            },
+            "block_id": "mememes",
+            "image_url": memeLinks[Math.floor(Math.random() * memeLinks.length)],
+            "alt_text": "yeet"
+          }
+        ]
+      });
+    }
+    // else it's a plaintext
+    else {
+      await say(picked_response);
+    }
+    console.log(`Responded to message from ${message.user}`);
   }
-  //if an empty string response is chosen--fill with meme attachment
-  else if (!picked_response){
-    await say({
-    blocks: [
-        {
-          "type": "image",
-          "title": {
-            "type": "plain_text",
-            "text": "yaet"
-          },
-          "block_id": "mememes",
-          "image_url": memeLinks[Math.floor(Math.random() * memeLinks.length)],
-          "alt_text": "yeet"
-        }
-      ]
-    });
-  }
-  // else it's a plaintext
-  else {
-    await say(picked_response);
-  }
-  console.log("Responded to message from " + message.user);
 });
 
 app.message(/(hwh[yae]).*/, async ({ message, context, say }) => {
-  // RegExp matches are inside of context.matches
-  // const greeting = context.matches[0];
-  const choices = ['I\'m loving the hhhhwh in that. thank you 🥰🥰',
-                 'F`ing delicious. Thank you for the beautiful hhwwwh. mHWaH'
-                ];
+  if(message.user != "USLACKBOT"){
+    // RegExp matches are inside of context.matches
+    // const greeting = context.matches[0];
+    const choices = ['I\'m loving the hhhhwh in that. thank you 🥰🥰',
+                   'F`ing delicious. Thank you for the beautiful hhwwwh. mHWaH'
+                  ];
 
-  await say(choices[Math.floor(Math.random() * choices.length)]);
-  console.log("Responded to message from " + message.user);
+    await say(choices[Math.floor(Math.random() * choices.length)]);
+    console.log(`Responded to message from ${message.user}`);
+  }
 });
 
 app.message(/([^h]what|[^h]why|[^h]where).*/, async ({ message, context, say }) => {
-  // RegExp matches are inside of context.matches
-  const greeting = context.matches[0];
+  if(message.user != "USLACKBOT"){
+    // RegExp matches are inside of context.matches
+    const greeting = context.matches[0];
 
-  if(Math.floor(Math.random() * 4)%3 == 0){
-    await say('*H'+greeting);
+    if(Math.floor(Math.random() * 4)%3 == 0){
+      await say('*H'+greeting);
+    }
+
+    console.log(`Responded to message from ${message.user}`);
   }
-
-  console.log("Responded to message from " + message.user);
-
 });
 
 (async () => {
