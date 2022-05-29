b37f79c11e039d4883bb64eb67a82b41ca429049
Myung Chang Lee (Noah Lee) <20549315+noahlee577@users.noreply.github.com>
2022-05-29 03:37:31 -0700

    switched from socket to https

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const { App } = require('@steve');
+const { App } = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
-  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
-  // you still need to listen on some port!
-  port: process.env.PORT || 3000
 });
 
 // Listens to incoming messages that contain "hello"
@@ -18,7 +13,7 @@ app.message('hello', async ({ message, say }) => {
 
 (async () => {
   // Start your app
-  await app.start();
+  await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log('Steve is online :)');
 })();
